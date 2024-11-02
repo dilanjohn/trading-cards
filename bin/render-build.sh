4079d8516b1cd34e2af7a62d69309891d6eb8904
dilanjohn <dilan.pourmoezzi@gmail.com>
2024-11-02 12:38:43 -0700

    changed render build files to match docs

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,14 +2,9 @@
 # exit on error
 set -o errexit
 
-# Install dependencies
-bundle config set --local deployment 'true'
-bundle config set --local without 'development test'
 bundle install
-
-# Build commands
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
 
 # Database setup
-bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup 
+#bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup 
